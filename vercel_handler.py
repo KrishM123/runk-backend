241e51564cf1502fab8d6630cbe998b02b5c4be3
KrishM123 <kahnscatterkrish@gmail.com>
2024-09-15 03:06:30 -0400

    update

--- vercel_handler.py
+++
@@ -1,8 +0,0 @@
-from apis import app
-from flask import Flask
-from werkzeug.middleware.proxy_fix import ProxyFix
-
-app.wsgi_app = ProxyFix(app.wsgi_app)
-
-def handler(request, response):
-    return app(request, response)
